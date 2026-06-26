cab410df29d7911323db530f9a5ed04c5702c85e
Revanth <revanthpatnani@gmail.com>
2026-06-26 19:05:04 +0530

    add next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
+  swcMinify: true,
+  basePath: "/tiny",
 };
 
 export default nextConfig;
